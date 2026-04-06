b98a45b98043e113821d8f8e5f95e13269a5bd72
Jocelyn <jocelynloisy500@gmail.com>
2026-04-06 22:33:41 +0200

    feat(firebase): add Firebase Web SDK initialization and environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBD3bi_23TX3SC-TEv8NdOkbc6hwdzhqQw',
+    authDomain: 'parkfinder-492516.firebaseapp.com',
+    projectId: 'parkfinder-492516',
+    storageBucket: 'parkfinder-492516.firebasestorage.app',
+    messagingSenderId: '507073097207',
+    appId: '1:507073097207:web:d8b9cf60913b70944212f2',
+    measurementId: 'G-6GYVWSJS8H',
+  },
+}
