d57bc58b3af65e5fe1b1092f8ddc33cdac47c349
Jocelyn <jocelynloisy500@gmail.com>
2026-04-09 18:23:46 +0200

    refactor(guardians): rename guardians for clarity

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,60 +1,55 @@
 import { Routes } from '@angular/router'
-import { Shell } from './layout/shell/shell'
-import { authGuard } from './core/guards/auth'
+import { authGuard } from './core/guards/authGuard'
 import { noAuthGuard } from './core/guards/noAuthGuard'
+import { redirectGuard } from './core/guards/redirectGuard'
 
 export const routes: Routes = [
   // Layout Auth (login/register)
   {
     path: '',
+    canActivate: [redirectGuard],
+    loadComponent: () =>
+      import('./layout/shell/shell').then(m => m.Shell)
+  },
+  {
+    path: 'login',
+    canActivate: [noAuthGuard],
+    loadComponent: () =>
+      import('./features/auth/auth-layout/auth-layout').then(m => m.AuthLayout),
     children: [
       {
-        path: 'login',
-        canActivate: [noAuthGuard],
+        path: '',
         loadComponent: () =>
-          import('./features/auth/auth-layout/auth-layout').then(m => m.AuthLayout),
-        children: [
-          {
-            path: '',
-            loadComponent: () =>
-              import('./features/auth/login/login').then(m => m.Login),
-          },
-        ],
+          import('./features/auth/login/login').then(m => m.Login),
       },
+    ],
+  },
+  {
+    path: 'register',
+    canActivate:[noAuthGuard],
+    loadComponent: () =>
+      import('./features/auth/auth-layout/auth-layout').then(m => m.AuthLayout),
+    children: [
       {
-        path: 'register',
-        canActivate:[noAuthGuard],
+        path: '',
         loadComponent: () =>
-          import('./features/auth/auth-layout/auth-layout').then(m => m.AuthLayout),
-        children: [
-          {
-            path: '',
-            loadComponent: () =>
-              import('./features/auth/register/register').then(m => m.Register),
-          },
-        ],
+          import('./features/auth/register/register').then(m => m.Register),
       },
     ],
   },
 
   // Layout Shell (navbar + footer + map)
   {
-    path: '',
+    path: 'map',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./layout/shell/shell').then(m => m.Shell),
-    canActivate: [authGuard],
     children: [
       {
-        path: 'map',
-        loadComponent: () =>
-          import('./features/map/map-view/map-view').then(m => m.MapView),
-      },
-      {
         path: '',
-        redirectTo: 'map',
-        pathMatch: 'full',
-      },
-    ],
-  },
+        loadComponent: () =>
+          import('./features/map/map-view/map-view').then(m => m.MapView)
+      }
+    ]
+  }
 ]
-
