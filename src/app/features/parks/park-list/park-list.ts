d8502b521ad073f7a0b58c1512480ccdd9b91438
Jocelyn <jocelynloisy500@gmail.com>
2026-04-10 21:33:19 +0200

    chore(cleanup): remove material icons link from index.html
    - Suppression du lien Material Icons hérité d’Angular Material
    - Conservation des Google Fonts nécessaires au design system
    - Nettoyage final du fichier index.html

--- src/app/features/parks/park-list/park-list.ts
+++ src/app/features/parks/park-list/park-list.ts
@@ -1,12 +1,11 @@
 import { Component, inject } from '@angular/core'
 import { NgFor } from '@angular/common'
-import { MatListModule } from '@angular/material/list'
 import { ParksService } from '../../../core/services/parks'
 
 @Component({
   selector: 'app-park-list',
   standalone: true,
-  imports: [NgFor, MatListModule],
+  imports: [NgFor],
   templateUrl: './park-list.html',
   styleUrl: './park-list.scss',
 })
